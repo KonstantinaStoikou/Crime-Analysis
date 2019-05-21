765c240d47f4d8cedebea60d9f917b4a1697448e
Konna <tinasto97@gmail.com>
2019-05-21 17:14:06 +0300

    Add plot making in seperate function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+from plot_data import make_plot
 
 # SET YOUR LOCAL PATHS to be used later in the program
 dataset_path = './crime.csv'
@@ -24,45 +23,20 @@ print(df['SHOOTING'].unique())
 df['SHOOTING'].fillna('N', inplace=True)
 print(df['SHOOTING'].unique())
 
-year_group_df = df.groupby(['YEAR'])['INCIDENT_NUMBER'].count()
-month_group_df = df.groupby(['MONTH'])['INCIDENT_NUMBER'].count()
-day_group_df = df.groupby(['DAY_OF_WEEK'])['INCIDENT_NUMBER'].count()
-distr_group_df = df.groupby(['DISTRICT'])['INCIDENT_NUMBER'].count()
+year_group = df.groupby(['YEAR'])['INCIDENT_NUMBER'].count()
+month_group = df.groupby(['MONTH'])['INCIDENT_NUMBER'].count()
+day_group = df.groupby(['DAY_OF_WEEK'])['INCIDENT_NUMBER'].count()
+distr_group = df.groupby(['DISTRICT'])['INCIDENT_NUMBER'].count()
 
-print(year_group_df)
-print(month_group_df)
-print(day_group_df)
 # change order of rows (based on week days order)
-day_group_df = day_group_df.reindex(
+day_group = day_group.reindex(
     index=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-print(day_group_df)
+print(day_group)
 
 # make plots
-sns.set(style="darkgrid")
-
-norm = plt.Normalize(0, year_group_df.values.max())
-colors = plt.cm.Blues(norm(year_group_df.values))
-ax = sns.barplot(year_group_df.index,
-                 year_group_df.values, palette=colors)
-ax.set(ylabel='Crime frequency', xlabel='Year')
-plt.title('Crime frequency by year')
-plt.savefig(images_path + 'year.png', bbox_inches="tight")
-plt.close()
-
-norm = plt.Normalize(0, month_group_df.values.max())
-colors = plt.cm.Blues(norm(month_group_df.values))
-ax = sns.barplot(month_group_df.index,
-                 month_group_df.values, palette=colors)
-ax.set(ylabel='Crime frequency', xlabel='Month')
-plt.title('Crime frequency by month')
-plt.savefig(images_path + 'month.png', bbox_inches="tight")
-plt.close()
-
-norm = plt.Normalize(0, day_group_df.values.max())
-colors = plt.cm.Blues(norm(day_group_df.values))
-ax = sns.barplot(day_group_df.index,
-                 day_group_df.values, palette=colors)
-ax.set(ylabel='Crime frequency', xlabel='Day')
-plt.title('Crime frequency by day')
-plt.savefig(images_path + 'day.png', bbox_inches="tight")
-plt.close()
+make_plot(year_group, 'Year', 'Crime frequency',
+          'Crime frequency by year', images_path, 'year.png')
+make_plot(month_group, 'Month', 'Crime frequency',
+          'Crime frequency by month', images_path, 'month.png')
+make_plot(day_group, 'Day', 'Crime frequency',
+          'Crime frequency by day', images_path, 'day.png')
---
+++ plot_data.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def make_plot(ser, xlabel, ylabel, title, path, image_name):
+    """ Make a bar plot on the given series. """
+    sns.set(style="darkgrid")
+
+    norm = plt.Normalize(0, ser.values.max())
+    colors = plt.cm.Blues(norm(ser.values))
+    ax = sns.barplot(ser.index,
+                     ser.values, palette=colors)
+    ax.set(ylabel=ylabel, xlabel=xlabel)
+    plt.title(title)
+    # save plot to an image
+    plt.savefig(path + image_name, bbox_inches="tight")
+    plt.close()
