88da65f678bed59b001d60b22328f20f84661bdb
Konna <tinasto97@gmail.com>
2019-05-21 16:59:59 +0300

    Make plots

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ images_path = './stat_images/'
 
 # read data and store them in a dataframe
 df = pd.read_csv(dataset_path, engine='python')
-print(df['SHOOTING'].head(10))
+print(df.head(10))
 print(df.dtypes)
 print(df.columns.values)
 
@@ -29,11 +29,40 @@ month_group_df = df.groupby(['MONTH'])['INCIDENT_NUMBER'].count()
 day_group_df = df.groupby(['DAY_OF_WEEK'])['INCIDENT_NUMBER'].count()
 distr_group_df = df.groupby(['DISTRICT'])['INCIDENT_NUMBER'].count()
 
-print(year_group_df.sum())
-print(month_group_df.sum())
+print(year_group_df)
+print(month_group_df)
 print(day_group_df)
-print(type(distr_group_df))
+# change order of rows (based on week days order)
+day_group_df = day_group_df.reindex(
+    index=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+print(day_group_df)
+
+# make plots
 sns.set(style="darkgrid")
-sns.barplot(year_group_df.index, year_group_df.values)
-# plt.show()
-plt.savefig(images_path + 'year.png')
+
+norm = plt.Normalize(0, year_group_df.values.max())
+colors = plt.cm.Blues(norm(year_group_df.values))
+ax = sns.barplot(year_group_df.index,
+                 year_group_df.values, palette=colors)
+ax.set(ylabel='Crime frequency', xlabel='Year')
+plt.title('Crime frequency by year')
+plt.savefig(images_path + 'year.png', bbox_inches="tight")
+plt.close()
+
+norm = plt.Normalize(0, month_group_df.values.max())
+colors = plt.cm.Blues(norm(month_group_df.values))
+ax = sns.barplot(month_group_df.index,
+                 month_group_df.values, palette=colors)
+ax.set(ylabel='Crime frequency', xlabel='Month')
+plt.title('Crime frequency by month')
+plt.savefig(images_path + 'month.png', bbox_inches="tight")
+plt.close()
+
+norm = plt.Normalize(0, day_group_df.values.max())
+colors = plt.cm.Blues(norm(day_group_df.values))
+ax = sns.barplot(day_group_df.index,
+                 day_group_df.values, palette=colors)
+ax.set(ylabel='Crime frequency', xlabel='Day')
+plt.title('Crime frequency by day')
+plt.savefig(images_path + 'day.png', bbox_inches="tight")
+plt.close()
